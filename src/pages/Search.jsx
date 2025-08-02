243fdf1036b8abb7b9f401babd4c9feaaa5a3a22
Vignesh-Ops <vigneshvickymailid@gmail.com>
2025-08-02 04:54:29 -0400

    Added search and sort products

---
+++ src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Inputfield from '../app/ui/Inputfield'
+
+const Search = () => {
+
+
+    return (
+        <>
+        <Inputfield  type='search'/>
+        </>
+    )
+}
+
+export default Search
